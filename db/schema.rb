d7d7dfeb77bcbec4cfe52f98e70848689fbea005
lytay <lytaytouch@gmail.com>
2015-10-05 11:10:10 +0700

    Add username to seed file and migrate schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151002091043) do
+ActiveRecord::Schema.define(version: 20151005030001) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,9 +29,11 @@ ActiveRecord::Schema.define(version: 20151002091043) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "username"
   end
 
   add_index "administrators", ["email"], name: "index_administrators_on_email", unique: true, using: :btree
   add_index "administrators", ["reset_password_token"], name: "index_administrators_on_reset_password_token", unique: true, using: :btree
+  add_index "administrators", ["username"], name: "index_administrators_on_username", unique: true, using: :btree
 
 end
