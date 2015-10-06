30971b00897efa356f3fd5b7e90ad775f56cf6bf
lytay <lytaytouch@gmail.com>
2015-10-06 17:14:50 +0700

    Update customize devise using username or email login

--- app/models/administrator.rb
+++ app/models/administrator.rb
@@ -2,5 +2,24 @@ class Administrator < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :authentication_keys => [:login]
+
+  validates_format_of :username, with: /[a-zA-Z0-9_\.]/
+
+  validates :username, :presence => true, :uniqueness => { :case_sensitive => false } # etc.
+
+  validates_uniqueness_of :username
+
+  attr_accessor :login
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      where(conditions.to_h).first
+    end
+  end
+
 end
