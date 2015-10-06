bd47b27d4ed420c31c0206ebe994da13cdda120b
lytay <lytaytouch@gmail.com>
2015-10-06 17:14:50 +0700

    Update customize devise using username or email login

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
-  before_action :authenticate_administrator!
+  # before_action :authenticate_administrator!
   def index
+    @user = current_administrator
   end
 end
